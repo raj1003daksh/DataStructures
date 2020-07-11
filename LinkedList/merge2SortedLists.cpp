5ab7cf71a2f8ee0efa70b115bf7ef63a2d5b81ca
Raj <raj1003daksh@gmail.com>
2020-07-11 20:45:51 +0530

    Added merge2SortedList.cpp

---
+++ LinkedList/merge2SortedLists.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ListNode* Solution::mergeTwoLists(ListNode* A, ListNode* B) {
+    ListNode *head,*fakehead;
+    head=new ListNode(0);
+    fakehead=new ListNode(-1);
+    fakehead->next=head;
+    ListNode *nptr=A,*lptr=B;
+    while(nptr!=NULL&&lptr!=NULL)
+    {
+        if(nptr->val<lptr->val)
+        {
+            head->next=new ListNode(nptr->val);
+            head=head->next;
+            nptr=nptr->next;
+        }
+        else
+        {
+            head->next=new ListNode(lptr->val);
+            head=head->next;
+            lptr=lptr->next;
+        }
+    }
+    if(nptr!=NULL)
+    {
+        while(nptr!=NULL)
+        {
+           head->next=new ListNode(nptr->val);
+           head=head->next;
+           nptr=nptr->next;
+        }
+    }
+    if(lptr!=NULL)
+    {
+        while(lptr!=NULL)
+        {
+            head->next=new ListNode(lptr->val);   
+            head=head->next;
+            lptr=lptr->next;
+        }
+    }
+    return fakehead->next->next;
+}
